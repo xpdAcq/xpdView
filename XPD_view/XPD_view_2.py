00171aad4d39f295fb2d921a9b5355855138b96b
Joseph Kaming-Thanassi <josephkamingthanassi@gmail.com>
2016-08-17 09:11:21 -0400

    fixed bug where roi would not be assigned to the RPP popup window

--- XPD_view/XPD_view_2.py
+++ XPD_view/XPD_view_2.py
@@ -255,7 +255,7 @@ class Display2(QtGui.QMainWindow):
         multi.setLayout(layout)
         self.plot_dock.setWidget(multi)
 
-    def new_r_rep(self, selection):
+    def new_r_rep(self, selection, x_min, x_max, y_min, y_max):
         try:
             popup_window = QtGui.QDialog(self)
             popup_window.setWindowTitle(selection)
@@ -273,12 +273,16 @@ class Display2(QtGui.QMainWindow):
             vbox.addWidget(canvas)
             popup_window.setLayout(vbox)
             popup_window.show()
-            self.rpp_list[-1].show()
+            self.rpp_list[idx].x_start = x_min
+            self.rpp_list[idx].x_stop = x_max
+            self.rpp_list[idx].y_start = y_min
+            self.rpp_list[idx].y_stop = y_max
+            self.rpp_list[idx].show()
             popup_window.exec_()
         except Exception:
             print("error creating a new rpp window")
         finally:
-            self.rpp_list.pop(idx)
+            self.rpp_list.__delitem__(idx)
 
     def update_r_rep(self, new_data):
         """
@@ -546,7 +550,9 @@ class Display2(QtGui.QMainWindow):
         new_plot_btn.setText("Plot in new window")
         new_plot_btn.clicked.connect(menu.close)
         new_plot_btn.clicked.connect(lambda: self.set_analysis_type(analysis_selector.currentIndex()))
-        new_plot_btn.clicked.connect(lambda: self.new_r_rep(analysis_selector.currentText()))
+        new_plot_btn.clicked.connect(lambda: self.new_r_rep(analysis_selector.currentText(),
+                                                            x_lim_min.value(), x_lim_max.value(),
+                                                           y_lim_min.value(), y_lim_max.value()))
 
         # defining layout
         vbox.addStretch()
