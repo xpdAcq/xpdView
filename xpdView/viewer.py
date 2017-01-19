600911137fe49773069b16573d3628f7b5ea5a3d
Timothy Liu <chbrahmsliu@gmail.com>
2017-01-19 15:34:43 -0500

    DEV: add specific matplotlib backend

--- xpdView/viewer.py
+++ xpdView/viewer.py
@@ -22,6 +22,8 @@ import numpy as np
 from PyQt4 import QtGui, QtCore
 from collections import OrderedDict
 
+import matplotlib
+matplotlib.use('Qt4Agg')
 
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolBar
