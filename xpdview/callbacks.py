a61ef4cdb6ecc1cb70057a996ae048704350ede6
christopher <cjwright4242@gmail.com>
2017-08-23 14:18:43 -0400

    DOC: document waterfall callback

--- xpdview/callbacks.py
+++ xpdview/callbacks.py
@@ -1,16 +1,21 @@
 from bluesky.callbacks.broker import BrokerCallbackBase
 from .waterfall import Waterfall
-import matplotlib.pyplot as plt
 
 
 class LiveWaterfall(BrokerCallbackBase):
     """
-    Stream 2D images in a cross-section viewer.
+    Stream 1D images in a waterfall viewer.
 
     Parameters
     ----------
-    field : string
-        name of data field in an Event
+    x_name : str
+        field name for x dimension
+    y_name: str
+        field name for y dimension
+    db: databroker.Broker instance
+        The Broker to fill the events if events are not filled
+    units: tuple of str
+        The units for the x and y axes
     """
 
     def __init__(self, x_name, y_name, db=None, units=None):
@@ -20,7 +25,10 @@ class LiveWaterfall(BrokerCallbackBase):
         self.x_name = x_name
         self.y_name = y_name
         self.units = units
-        self.fig = plt.figure()
+
+        from matplotlib.figure import Figure
+        self.fig = Figure()
+
         self.wf = Waterfall(fig=self.fig, unit=self.units)
         self.i = 0
 
