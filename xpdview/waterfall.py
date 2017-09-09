c94ddcb742324eb16bbf400cd658f21e8d92d349
Timothy Liu <chbrahmsliu@gmail.com>
2017-09-09 16:16:21 -0400

    REF/FIX: performance-related tweaks on waterfall plots
    changes includes:
    1) only compute min and max of x-, y-array_list when updating array
    data but not during redraw
    2) fix the error of set x-, y-label

--- xpdview/waterfall.py
+++ xpdview/waterfall.py
@@ -2,12 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 
-
-def _normalize(self, array, max_val, min_val):
-    """core function to normalize a ndarray"""
-    return np.subtract(array, min_val) / np.subtract(max_val, min_val)
-
-
 class Waterfall:
     """class holds data and generate watefall plot
 
@@ -46,7 +40,6 @@ class Waterfall:
         self.int_data_list = int_data_list
         self.ax = self.fig.add_subplot(111)
         self.unit = unit
-
         # flag to prevent update
         self.halt = False
         # add sliders, which store information
@@ -63,6 +56,7 @@ class Waterfall:
         self.x_offset_slider.on_changed(self.update_x_offset)
         # init
         self.update(self.key_list, self.int_data_list, refresh=True)
+
 
     def update(self, key_list=None, int_data_list=None, refresh=False):
         """top method to update information carried by class and plot
@@ -88,6 +82,7 @@ class Waterfall:
             self.int_data_list = []
         self.key_list.extend(key_list)
         self.int_data_list.extend(int_data_list)
+        self._adapt_data_list(int_data_list)
         # generate plot
         self.halt = False
         self._update_plot()  # use current value of x,y offset
@@ -96,17 +91,15 @@ class Waterfall:
         """method to return statefull information of 1D data list"""
         x_array_list = []
         y_array_list = []
+        # parse
         for x, y in int_data_list:
             x_array_list.append(x)
             y_array_list.append(y)
-        y_max = np.max(y_array_list)
-        y_min = np.min(y_array_list)
-        y_dist = y_max - y_min
-        x_max = np.max(x_array_list)
-        x_min = np.min(x_array_list)
-        x_dist = x_max - x_min
-        return (x_array_list, y_array_list, y_min, y_max,
-                y_dist, x_min, x_max, x_dist)
+        self.x_array_list = x_array_list
+        self.y_array_list = y_array_list
+        # stateful information
+        self.y_dist = np.max(y_array_list) - np.min(y_array_list)
+        self.x_dist = np.max(x_array_list) - np.min(x_array_list)
 
     def on_plot_hover(self, event):
         """callback to show legend when click on one of curves"""
@@ -120,24 +113,23 @@ class Waterfall:
         """core method to update x-, y-offset sliders"""
         self.ax.set_facecolor('w')
         self.ax.cla()
+        # remain current offset
         if not x_offset_val:
             x_offset_val = self.x_offset_slider.val
         if not y_offset_val:
             y_offset_val = self.y_offset_slider.val
         # get stateful info
-        state = self._adapt_data_list(self.int_data_list)
-        x_array_list, y_array_list, \
-        y_min, y_max, y_dist, x_min, x_max, x_dist = state
-        for ind, el in enumerate(zip(x_array_list, y_array_list)):
+        for ind, el in enumerate(zip(self.x_array_list,
+                                     self.y_array_list)):
             x, y = el
-            self.ax.plot(x + x_dist * ind * x_offset_val,
-                         y + y_dist * ind * y_offset_val,
+            self.ax.plot(x + self.x_dist * ind * x_offset_val,
+                         y + self.y_dist * ind * y_offset_val,
                          label=self.key_list[ind], picker=5)
         self.ax.autoscale()
         if self.unit:
             xlabel, ylabel = self.unit
-            self.ax.set_xlabel = xlabel
-            self.ax.set_ylabel = ylabel
+            self.ax.set_xlabel(xlabel)
+            self.ax.set_ylabel(ylabel)
         self.canvas.draw_idle()
 
     def update_y_offset(self, val):
@@ -155,10 +147,10 @@ class Waterfall:
         """
         ax.cla()
         ax.text(.5, .5,
-                '{}'.format("We couldn't find reduced data in directory "
-                            "currently set.\nReducded data are generated "
-                            "by proper calibration and integration.\n"
-                            "Please go to our documentation for more details:\n"
+                '{}'.format("No reduced data can be generated.\n"
+                            "Proper calibration is required for "
+                            "data reduction.\n"
+                            "Please refer to our documentation for more details:\n"
                             "http://xpdacq.github.io/quickstart.html"),
                 ha='center', va='center', color='w',
                 transform=ax.transAxes, size=11)
