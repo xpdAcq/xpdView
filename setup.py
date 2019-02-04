a8a5fca21c4a3e8f86da3dc82b68e12185002710
Christopher J. Wright <cjwright4242@gmail.com>
2019-02-03 19:48:15 -0500

    bumped version to 0.5.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xpdview',
-    version='0.4.1',
+    version='0.5.0',
     packages=find_packages(),
     description='Visualization Code for Beam line',
     zip_safe=False,
